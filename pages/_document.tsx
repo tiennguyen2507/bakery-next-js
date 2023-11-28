17460ee61575ba264a667aa0fac29990baf2432e
Nguyễn Lê Đình Tiên <nguyenledinhtien2507@gmail.com>
2023-11-28 22:54:36 +0700

    update-meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
         <Head />
         <body>
           <Main />
